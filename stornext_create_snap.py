19ea6c577a97736f70cbf13b45874ee037b85584
Darshan <darshan@bigscal.com>
2025-04-30 15:22:01 +0530

    Refactor argument parsing and error handling in Stornext snapshot script

--- stornext_create_snap.py
+++ stornext_create_snap.py
@@ -1,23 +1,19 @@
-from pprint import pprint
-import os
 import requests
-import subprocess
-import json
-import platform
-import time
-import sys
-import time    
 import argparse
-from configparser import ConfigParser
-import plistlib
-import ssl
 import base64
 import urllib3
+import json
 
 urllib3.disable_warnings()
 
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--projectname', required=True)
+    parser.add_argument('-s', '--sourcepath', required=True)
+    parser.add_argument('--hostname', required=True)
+    parser.add_argument('-o', '--port', required=True)
+    parser.add_argument('-t', '--token', required=True)
+    args = parser.parse_args()
 
     params = {
         'details': 'sa',
@@ -27,31 +23,21 @@ if __name__ == '__main__':
         'exclude': '.*DS_Store'
     }
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--projectname', required = True, help = 'Project we are performing scan for.')
-    parser.add_argument('-s', '--sourcepath', required = True, help = 'Source path (needed for Stornext.')
-    parser.add_argument('--hostname', required = True, help = 'Source hostname needed for Stornext.')
-    parser.add_argument('-o', '--port', required = True, help = 'Source port (needed for Stornext.')
-    parser.add_argument('-t', '--token', required = True, help = 'Source token (needed for Stornext.')
-    args = parser.parse_args()
-
-    output_dict = {}
-
-    token_decoded = args.token
-    token_bytes = token_decoded.encode('ascii')
-    base64_bytes = base64.b64encode(token_bytes)
-    encoded_token = base64_bytes.decode('ascii')
+    token_bytes = args.token.encode('ascii')
+    encoded_token = base64.b64encode(token_bytes).decode('ascii')
 
     url = f'https://{args.hostname}:{args.port}/api/metadb/v1/marker'
-    auth_header = f'Basic {encoded_token}'
-    header = {'Authorization' : auth_header}
+    headers = {'Authorization': f'Basic {encoded_token}'}
 
-    response = requests.get(url, headers=header,  params=params, verify=False)
-    if response.status_code != 200:
-        print("Error contacting Stornext: " + str(response.status_code))
-        exit(22)
+    try:
+        response = requests.get(url, headers=headers, params=params, verify=False)
+        if response.status_code != 200:
+            print(json.dumps({"error": f"Stornext marker request failed with status code {response.status_code}"}))
+            exit(22)
 
-    new_snapshot_id = response.json()['marker']
-    print(new_snapshot_id)
-    exit(0)
-
+        data = response.json()
+        print(json.dumps({"marker": data.get("marker")}))
+        exit(0)
+    except Exception as e:
+        print(json.dumps({"error": str(e)}))
+        exit(1)
